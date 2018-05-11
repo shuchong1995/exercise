361dbc7f135cb689c27f8c2b835232ecda3ce34f
shu19 <shu1995@shu.edu.cn>
2018-05-11 21:11:57 +0800

    exercise

---
+++ seven.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+def printtime(str):
+    "打印字符串"
+    print (str)
+    return
+printtime("调用当前函数")
+
+
+
+
+
+def change(list):
+    list.append([1,2,3])
+    print(list)
+    return
+
+
+list = [10,20,30]
+change(list)
+
+
+
